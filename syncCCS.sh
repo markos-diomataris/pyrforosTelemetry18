d9717f7d15e6cf01714cfe74b875d40d4a75bd82
Markos-NTUA <diomataris.markos@gmail.com>
2018-05-30 19:14:15 +0300

    updated sync scripts

---
+++ syncCCS.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "**BE CAUTIOUS this script may fuckup your project dir**"
+echo "Are you sure you want to copy back to the project dir?"
+read ans
+if [[ "$ans" != "y" ]]; then
+  echo "Exiting"
+  exit 0
+fi
+echo "cd piccolo_code"
+cd piccolo_code || exit 1
+echo "cp -r . /home/markos/ti/C2000Ware_1_00_03_00_Software/device_support/f2802x/examples/drivers/adc_soc/" 
+cp -r . /home/markos/ti/C2000Ware_1_00_03_00_Software/device_support/f2802x/examples/drivers/adc_soc || exit 1
+echo "DONE"
